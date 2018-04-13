965375a40109bcb32d62af8f02d330e500e3f455
shaoqing <kusanyao@qq.com>
2018-04-13 17:57:00 +0800

    添加模板

--- src/main/main.go
+++ src/main/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "time"
-    "log"
+	"fmt"
+	"log"
+	"net/http"
+	"html/template"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello Kusanyao!") //这个写入到w的是输出到客户端的
+var port = "9090" // 定义端口
+
+func home(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("./src/views/login.tpl")
+	log.Println(t.Execute(w, nil))
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	fmt.Fprintf(w, "register")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	fmt.Fprintf(w, "login")
+}
+
+func regRouter() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 }
 
 func main() {
-    http.HandleFunc("/", sayhelloName) //设置访问的路由
-    err := http.ListenAndServe(":9090", nil) //设置监听的端口
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	// 设置访问的路由
+	regRouter()
+	fmt.Println("server start " + port)
+	err := http.ListenAndServe(":"+port, nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
